6fff466e127c9e68246397e84098edd5dc9ae394
kondaroshini <123068795+kondaroshini@users.noreply.github.com>
2023-06-20 22:35:52 +0530

    Add files via upload

---
+++ Addition of 2 Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner s=new Scanner(System.in);
+	    System.out.println("Enter the value of a::");
+	    int a=s.nextInt();
+	    System.out.println("Enter the value of b:");
+	    int b=s.nextInt();
+	    System.out.println(a+b);
+	}
+}
